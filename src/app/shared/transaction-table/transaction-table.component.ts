8b9b1875c0412014189e527f142d272d4f80b30e
Vojtech Hrbas <hrbas@edhouse.cz>
2019-01-04 15:10:52 +0100

    Enhanced transaction table with paginator, sorting and filtering

--- src/app/shared/transaction-table/transaction-table.component.ts
+++ src/app/shared/transaction-table/transaction-table.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 import { Transaction } from '../domain/transaction';
 import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
 import { TransactionService } from '../transaction.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-table',
   templateUrl: './transaction-table.component.html',
-  styleUrls: ['./transaction-table.component.scss']
+  styleUrls: ['./transaction-table.component.scss'],
+  providers: [DatePipe]
 })
 export class TransactionTableComponent implements OnInit {
+
+  dateFormat = 'dd-MM-yyyy';
 
   displayedColumns = [
     'date',
@@ -24,10 +28,25 @@ export class TransactionTableComponent implements OnInit {
 
   dataSource = new MatTableDataSource();
 
-  constructor(public dialog: MatDialog, private transactionService: TransactionService) {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(public dialog: MatDialog, private transactionService: TransactionService, private datePipe: DatePipe) {
   }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource.filterPredicate = (data: Transaction, filter: string) =>
+      !filter
+      || (data.fromAccount && data.fromAccount.toLowerCase().includes(filter))
+      || (data.toAccount && data.toAccount.toLowerCase().includes(filter))
+      || (data.reference && data.reference.toLowerCase().includes(filter))
+      || (data.description && data.description.toLowerCase().includes(filter))
+      || (data.amount && data.amount.toString().includes(filter))
+      || (data.date && this.datePipe.transform(data.date, this.dateFormat).includes(filter));
+
     this.transactionService.getTransactions().subscribe(data => {
       this.dataSource.data = data;
     });
@@ -38,6 +57,9 @@ export class TransactionTableComponent implements OnInit {
       height: '350px',
       data: transaction
     });
+  }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
